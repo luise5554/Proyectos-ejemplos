f6455474dcd280d33ad1c557b1f8f9a3b1aa37e5
luise455 <luise455@gmail.com>
2011-11-28 15:42:24 -0500

    p

---
+++ a/src/luis/ejemplos/AActivity.java
@@ -0,0 +1,15 @@
+package luis.ejemplos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ///cambiado por luise455
+        //cambiado por luise5554
+    }
+}
