9f5517079716b18c5e9c45453cf42042d3656cae
luise5554 <luise5554@gmail.com>
2011-11-29 13:52:32 -0500

    Update proyectoA/a/src/luis/ejemplos/AActivity.java

--- proyectoA/a/src/luis/ejemplos/AActivity.java
+++ proyectoA/a/src/luis/ejemplos/AActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class AActivity extends Activity {
     /** modificado en luise455. otra vez y otra*/
+    /* ahora desde 5554*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
