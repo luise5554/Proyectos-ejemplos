6cd0eab86542674fe4a112a251a38f7c147bf07b
luise455 <luise455@gmail.com>
2011-11-29 12:20:48 -0500

    de

--- proyectoA/a/src/luis/ejemplos/AActivity.java
+++ proyectoA/a/src/luis/ejemplos/AActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class AActivity extends Activity {
-    /** modificado en luise5554. */
+    /** modificado en luise455. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
