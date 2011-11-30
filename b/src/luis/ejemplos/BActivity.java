fff56fc21d7525fc91ada6f372b1050328951e9f
luise455 <luise455@yahoo.es>
2011-11-30 10:55:12 -0500

    Update b/src/luis/ejemplos/BActivity.java

--- b/src/luis/ejemplos/BActivity.java
+++ b/src/luis/ejemplos/BActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class BActivity extends Activity {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created
+    luis1
+    . */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
